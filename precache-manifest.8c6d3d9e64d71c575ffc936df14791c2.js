160948ef0d5758d2f6ddc7a1997c15b47f652e15
Tureckiy <ev.zhdanov@gmail.com>
2020-06-14 23:52:10 +0300

    Updates

---
+++ precache-manifest.8c6d3d9e64d71c575ffc936df14791c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f0db852ce5e7a8a17b71accc1e351b",
+    "url": "/goit-react-hw-01-component/index.html"
+  },
+  {
+    "revision": "4a71a1306689424d7236",
+    "url": "/goit-react-hw-01-component/static/css/main.048996e3.chunk.css"
+  },
+  {
+    "revision": "6e9ead121b33dcfbabf3",
+    "url": "/goit-react-hw-01-component/static/js/2.ced73e1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-component/static/js/2.ced73e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a71a1306689424d7236",
+    "url": "/goit-react-hw-01-component/static/js/main.ee197e54.chunk.js"
+  },
+  {
+    "revision": "96fcb1a4fa202538acc7",
+    "url": "/goit-react-hw-01-component/static/js/runtime-main.ef33c514.js"
+  }
+]);
